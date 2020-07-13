755eb47c4114fbb9f44034b8ad8241712cc99b2d
Andrew Zabelin <a.zabelin1@yandex.ru>
2020-07-13 18:36:28 +0300

    add callbacks

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  validates :price, numericality: { greater_than: 0, allow_nil: true }
+  validates :name, :description, presence: true
+
+  after_initialize { p 'initialize custom' }
+  after_save { p 'save custom' }
+  after_create { p 'create custom' }
+  after_update { p 'update custom' }
+  after_destroy { p 'destroy custom' }
 end
